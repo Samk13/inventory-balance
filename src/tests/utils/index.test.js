6ca10053bcb0be61bee7d8e1d6e4ebcaeb5e31f7
Samk13 <samarbid13@gmail.com>
2020-11-16 01:14:02 +0100

    setup testing and pass 2 functions

---
+++ src/tests/utils/index.test.js
@@ -0,0 +1,47 @@
+const { objIterator, productIterator } = require("../../utils/index");
+
+test("iterate throw an abject should returns an array of the values", () => {
+  const values = {
+    createNewProduct: "c",
+    listProduct: "L",
+    sell: "S",
+    deliver: "l",
+    deliverAuto: "lAuto",
+    sellPackage: "SP",
+  };
+
+  expect(objIterator(values)).toStrictEqual([
+    "c",
+    "L",
+    "S",
+    "l",
+    "lAuto",
+    "SP",
+  ]);
+});
+
+test("iterate throw the product object and return an array of all obj.name with upper case ", () => {
+  stocks = [
+    {
+      id: 13233,
+      name: "test1",
+      category: "test",
+      quantity: 12,
+      price: 12,
+      delivered: 0,
+      sold: 0,
+      total: 0,
+    },
+    {
+      id: 13233,
+      name: "test2",
+      category: "test",
+      quantity: 12,
+      price: 12,
+      delivered: 0,
+      sold: 0,
+      total: 0,
+    },
+  ];
+  expect(productIterator(stocks)).toStrictEqual(["Test1", "Test2"]);
+});
