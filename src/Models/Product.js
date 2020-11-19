4ec5b041e2a99e2c3d52d140a26c1afaf646a4bd
Samk13 <samarbid13@gmail.com>
2020-11-19 01:41:14 +0100

    make Product test

--- src/Models/Product.js
+++ src/Models/Product.js
@@ -11,19 +11,6 @@ class Product {
     this.sold = parseInt(answers.sold) || 0;
     this.total = parseInt(answers.price) * parseInt(answers.sold) || 0;
   }
-
-  // getProduct() {
-  //   return {
-  //     id: this.id,
-  //     name: this.name,
-  //     category: this.category,
-  //     quantity: parseInt(this.quantity),
-  //     sold: parseInt(this.sold),
-  //     price: parseInt(this.price),
-  //     delivered: parseInt(this.delivered),
-  //     total: parseInt(this.total),
-  //   };
-  // }
 }
 
 module.exports = {
