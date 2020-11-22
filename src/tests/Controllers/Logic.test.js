785ddfaa250f381d641745e876fed029fd145a3f
Samk13 <samarbid13@gmail.com>
2020-11-22 03:53:19 +0100

    test calculateDiscount and sellProduct logic

--- src/tests/Controllers/Logic.test.js
+++ src/tests/Controllers/Logic.test.js
@@ -3,6 +3,7 @@ const {
   createNewProductLogic,
   listProductLogic,
   sellProdLogic,
+  calculateDiscount,
 } = require("../../Controllers/Logic");
 const { Product } = require("../../Models/Product");
 
@@ -72,9 +73,63 @@ describe("sellProdLogic", () => {
     sold: 0,
     total: 0,
   };
-  it(" if you pass wrong argument type", () => {
+
+  it("Make sure that the example has 0 values", () => {
+    expect(product.sold).toEqual(0);
+    expect(product.total).toEqual(0);
+    expect(product.quantity).toEqual(100);
+  });
+
+  it("Throw an error if you pass wrong argument type", () => {
     expect(() => {
       sellProdLogic(undefined, product);
     }).toThrow("the value you provide is not a number");
+  });
+
+  it("Add given value to the sold object val", () => {
+    sellProdLogic(1, product);
+    expect(product.sold).toEqual(1);
+  });
+
+  it("Subtract the given value from quantity", () => {
+    expect(product.quantity).toEqual(99);
+  });
+
+  it("Calculate the total by multiply the price by sold", () => {
+    expect(product.total).toEqual(10);
+  });
+});
+
+describe("Discount behaviour in sellProdLogic function", () => {
+  const prod = {
+    id: 2097198,
+    name: "packageItem2",
+    category: "general",
+    package: 1,
+    quantity: 100,
+    price: 10,
+    delivered: 0,
+    sold: 0,
+    total: 0,
+  };
+  it("Calculate the discount if the value is over 3", () => {
+    sellProdLogic(4, prod);
+    expect(prod.total).toEqual(32);
+  });
+});
+
+describe("calculateDiscount", () => {
+  it("Throw error if input is not a number", () => {
+    expect(() => calculateDiscount(undefined, "blaBla")).toThrow(
+      "price and discount should be a number!"
+    );
+  });
+
+  it("Return truthy value if it get the right data", () => {
+    expect(calculateDiscount(10, (1 / 4) * 100)).not.toBeFalsy;
+  });
+
+  it("Return the correct discount value and ceil it", () => {
+    expect(calculateDiscount(10, (1 / 4) * 100)).toEqual(8);
   });
 });
